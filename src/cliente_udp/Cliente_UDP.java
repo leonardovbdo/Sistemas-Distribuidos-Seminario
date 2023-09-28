6cfe730f1aac7bd9db630fa98d10e3b8f78040d3
leonardovbdo <odranoelrubi@gmail.com>
2023-09-28 15:44:21 -0300

    Implementação do download de arquivo

--- src/cliente_udp/Cliente_UDP.java
+++ src/cliente_udp/Cliente_UDP.java
@@ -1,29 +1,25 @@
 package cliente_udp;
 
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
 
 public class Cliente_UDP {
     public static void main(String[] args) {
-        String serverAddress = "127.0.1.1"; // Endereço IP ou nome de host do servidor
-        int serverPort = 12345; // Porta do servidor
+        try {
+            Socket clientSocket = new Socket("endereco_do_servidor", 12345); // Substitua pelo endereço real do servidor
 
-        try (Socket socket = new Socket(serverAddress, serverPort);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-
-            // Solicitação para baixar a versão mutualmente consistente do arquivo
-            out.println("mutualmente consistente");
-
-            // Recebe a resposta do servidor (mutualmente consistente ou desatualizada)
-            String response = in.readLine();
-            System.out.println("Servidor respondeu com: " + response);
-
-            // Recebe o conteúdo do arquivo, linha por linha
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
+            // Código para receber o arquivo do servidor
+            InputStream is = clientSocket.getInputStream();
+            FileOutputStream fos = new FileOutputStream("arquivo_recebido.txt");
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
             }
+            fos.close();
+            is.close();
+
+            clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/servidor_udp/Servidor_UDP.java
+++ src/servidor_udp/Servidor_UDP.java
@@ -1,61 +1,35 @@
 package servidor_udp;
 
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor_UDP {
     public static void main(String[] args) {
-        int port = 12345;
-        String stockPriceFile = "prices.txt"; // Nome do arquivo de preços das ações
-
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Servidor está esperando por conexões...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345); // Porta do servidor
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
+                System.out.println("Aguardando conexão...");
+                Socket clientSocket = serverSocket.accept(); // Aguarda a conexão do cliente
+                System.out.println("Conexão estabelecida com " + clientSocket.getInetAddress());
 
-                // Lógica para verificar desvio de R$ 0,02 e atualizar a réplica, se necessário
-                // Aqui, assumimos uma classe PriceUpdater que implementa essa lógica.
-
-                // Verifica desvio e atualiza a réplica, se necessário
-                boolean updateRequired = PriceUpdater.checkAndUpdate(stockPriceFile);
-
-                // Envia a versão apropriada do arquivo para o cliente
-                String response = updateRequired ? "mutualmente consistente" : "desatualizada";
-                sendFileToClient(clientSocket, stockPriceFile, response);
+                // Código para enviar o arquivo ao cliente
+                File fileToSend = new File("arquivo.txt");
+                FileInputStream fis = new FileInputStream(fileToSend);
+                OutputStream os = clientSocket.getOutputStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+                os.close();
+                fis.close();
 
                 clientSocket.close();
-                System.out.println("Cliente desconectado.");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void sendFileToClient(Socket clientSocket, String fileName, String response) throws IOException {
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-
-            // Envia a resposta ao cliente
-            out.println(response);
-
-            // Envia o conteúdo do arquivo
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                out.println(line);
-            }
-        }
-    }
-}
-
-class PriceUpdater {
-    public static boolean checkAndUpdate(String fileName) {
-        // Lógica para verificar se é necessário atualizar a réplica com base no desvio de R$ 0,02.
-        // Retorna true se a atualização for necessária, caso contrário, retorna false.
-        // Implemente sua lógica de verificação aqui.
-
-        // Neste exemplo simples, assumimos que a atualização é sempre necessária para fins de demonstração.
-        return true;
     }
 }
